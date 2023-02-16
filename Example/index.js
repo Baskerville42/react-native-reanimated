572a186b99f25523aa6d066644a2ca9100d3822f
Alexander Tartmin <alexander@tartmin.name>
2023-02-16 20:30:41 +0200

    Use App.tsx as default app

--- Example/index.js
+++ Example/index.js
@@ -1,11 +1,12 @@
 import { AppRegistry, Platform } from 'react-native';
 import { name as appName } from './app.json';
 
-// import App from './src/App';
-// AppRegistry.registerComponent(appName, () => App);
+// Uncomment AutofillExtension component if you need to check how Extension works without reanimated
+import App from './src/App';
+// import AutofillExtension from './src/AutofillExtension';
 
-import AutofillExtension from './src/AutofillExtension';
-AppRegistry.registerComponent(appName, () => AutofillExtension);
+AppRegistry.registerComponent(appName, () => App);
+// AppRegistry.registerComponent(appName, () => AutofillExtension);
 
 // TODO(Bacon): When `expo` has removed Updates, replace this with using the `expo` entry for better error handling
 if (Platform.OS === 'web') {
